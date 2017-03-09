b0b7d474a43fa02d763157665b06e49693c01030
Hershiv Haria <hershiv@kdaweb.com>
2017-03-09 17:17:06 +0000

    TDD in action! Fixed SQL statements, and corrected character case issues that caused values to not match up

--- src/main/CMUDatabaseManager.java
+++ src/main/CMUDatabaseManager.java
@@ -1,3 +1,5 @@
+import org.json.simple.parser.ParseException;
+
 import java.sql.*;
 import java.util.logging.Logger;
 
@@ -5,7 +7,7 @@ import java.util.logging.Logger;
  * Class to manage database connections, and any database calls.
  */
 public class CMUDatabaseManager {
-    
+
     private final static Logger logger = Logger.getLogger(CMUDatabaseManager.class.getName());
 
     // The names of our tables for tracking the votes for each candidate, and the votes by given user.
@@ -13,7 +15,7 @@ public class CMUDatabaseManager {
     private static String USER_TABLE_NAME = "VOTES_BY_USER";
 
     // The connection string used for the database. May update this to test performance.
-    private static String JDBC_CONN = "jdbc:sqllite:cmu.db";
+    private static String JDBC_CONN = "jdbc:sqlite:cmu.db";
 
     /**
      * Setup our database tables to hold our votes and our counts per user.
@@ -42,7 +44,7 @@ public class CMUDatabaseManager {
                                   " VOTE_COUNT INT NOT NULL)";
 
             String userTableSQL = "CREATE TABLE " + USER_TABLE_NAME + " " +
-                    "(USER_ID CHAR(50) PRIMARY KEY NOT NULL," +
+                    "(USER_ID CHAR(100) PRIMARY KEY NOT NULL," +
                     " VOTE_COUNT INT NOT NULL)";
 
             // Execute our statement to create our tables, and then close them out.
@@ -56,9 +58,17 @@ public class CMUDatabaseManager {
 
             logger.info("Tables set up successfully.");
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
             // Something unexpected happened
-            logger.severe("Error when setting up database tables");
+            logger.severe("Error when setting up database tables. Specifically an error with SQL.");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // The SQL driver isn't available. This causes problems.
+            logger.severe("Error when setting up database tables. Specifically: SQL JDBC driver not found.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            // Something else when wrong
+            logger.severe("Error when setting up database tables.");
             e.printStackTrace();
         }
     }
@@ -68,11 +78,11 @@ public class CMUDatabaseManager {
      * are 0.
      * @param candidateCount int must be greater than 1
      */
-    public static void populateVoteTable(int candidateCount) throws Exception {
+    public static void populateVoteTable(int candidateCount) {
 
         if (candidateCount < 1) {
             logger.warning("You didn't set a candidate count greater than 0.");
-            throw new Exception("candidateCount must be a postitive integer greater than or equal to 1.");
+            return;
         }
 
         logger.info("Populating vote count table.");
@@ -95,11 +105,13 @@ public class CMUDatabaseManager {
             // Loop the number of times as there are candidates - this allows us to have a variable number of candidates
             // when we deploy our application.
             for (int i = 1; i <= candidateCount; i++) {
-                sql += "(Candidate-" + i + ", 0)";
+                sql += "('candidate-" + i + "', 0)";
 
                 // Add a comma if we're not at the end of the loops
                 sql += i == candidateCount ? "" : ", ";
             }
+
+            logger.info(sql);
 
             // Execute our statement
             voteTableStmt.executeUpdate(sql);
@@ -110,9 +122,17 @@ public class CMUDatabaseManager {
 
             logger.info("Vote count table populated.");
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
             // Something unexpected happened
-            logger.severe("Error when populating vote count table");
+            logger.severe("Error when populating vote count table. Specifically an error with SQL.");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // The SQL driver isn't available. This causes problems.
+            logger.severe("Error when populating vote count table. Specifically: SQL JDBC driver not found.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            // Something else when wrong
+            logger.severe("Error when populating vote count table.");
             e.printStackTrace();
         }
     }
@@ -125,7 +145,8 @@ public class CMUDatabaseManager {
 
         // Prepare our variables for assignment
         Connection c = null;
-        Statement stmt = null;
+        Statement dropVoteTblStmt = null;
+        Statement dropUserTbjStmt = null;
 
         try {
             // Confirm we have the sqlite jdbc driver available
@@ -133,23 +154,148 @@ public class CMUDatabaseManager {
 
             // Open up the connection and statements
             c = DriverManager.getConnection(JDBC_CONN);
-            stmt = c.createStatement();
+            dropVoteTblStmt = c.createStatement();
+            dropUserTbjStmt = c.createStatement();
 
             // SQL to drop our tables
-            String sql = "DROP TABLE IF EXISTS " + VOTE_TABLE_NAME + ", " + USER_TABLE_NAME;
+            String sql = "DROP TABLE IF EXISTS ";
 
             // Execute the sql
-            stmt.execute(sql);
+            dropVoteTblStmt.execute(sql + VOTE_TABLE_NAME);
+            dropUserTbjStmt.execute(sql + USER_TABLE_NAME);
 
-            // Close our statement and connection
-            stmt.close();
+            // Close our statements and connection
+            dropVoteTblStmt.close();
+            dropUserTbjStmt.close();
             c.close();
 
             logger.info("Successfully dropped tables. Database clear.");
-        } catch (Exception e) {
+        } catch (SQLException e) {
             // Something unexpected happened
-            logger.severe("Error when populating vote count table");
+            logger.severe("Error when populating vote count table. Specifically an error with SQL.");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // The SQL driver isn't available. This causes problems.
+            logger.severe("Error when populating vote count table. Specifically: SQL JDBC driver not found.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            // Something else when wrong
+            logger.severe("Error when populating vote count table.");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Attempt to cast a vote for a user. Checks to make sure the user is still eligible to
+     * vote (less than 3 previous votes), and then attempts to update the tables if true. If not, returns a false to
+     * indicate the user is ineligible.
+     * @param userId
+     * @param candidateId
+     * @return int whether it was successful (0), ineligible (1), or there was an error (2)
+     * @throws Exception if there is an error when attempting to access the database
+     */
+    public static int castVote(String userId, String candidateId) throws Exception {
+        logger.info("Starting to cast vote");
+
+        // Prepare our variables for assignment
+        Connection c = null;
+        Statement voteCheckStmt = null;
+        PreparedStatement castVoteStmt = null;
+        PreparedStatement trackVoteStmt = null;
+
+
+        boolean oldAutoCommit = true;
+        int castRow = 0;
+        int trackRow = 0;
+
+        try {
+
+            // Confirm we have the sqlite jdbc driver available
+            Class.forName("org.sqlite.JDBC");
+
+            // Open up the connection and statements
+            c = DriverManager.getConnection(JDBC_CONN);
+
+            // Get the current setting for autocommit, for the transaction process later.
+            oldAutoCommit = c.getAutoCommit();
+
+            voteCheckStmt = c.createStatement();
+
+            String voteCheckSQL = "SELECT VOTE_COUNT FROM " + USER_TABLE_NAME + " WHERE USER_ID IS '" + userId+ "'";
+
+            ResultSet rs = voteCheckStmt.executeQuery(voteCheckSQL);
+
+            boolean userHasVoted = false;
+
+            // Check if the user has voted before
+            while (rs.next()) {
+                userHasVoted = true;
+                logger.info("Checking vote eligibility for user " + userId);
+                // Check if the user has voted 3 times already. If so, return without casting a new vote.
+                if (rs.getInt(userId) >= 3) {
+                    logger.info(userId + " is ineligible to vote.");
+                    return 1;
+                }
+            }
+
+            // The user is eligible to vote, so let's continue and make it happen.
+            logger.info("Casting vote and tracking.");
+
+            String castVoteSQL = "UPDATE " + VOTE_TABLE_NAME + " SET VOTE_COUNT = VOTE_COUNT + 1" +
+                    " WHERE CANDIDATE_ID IS ?";
+            String trackVoteSQL;
+            if (userHasVoted) {
+                trackVoteSQL = "UPDATE " + USER_TABLE_NAME + " SET VOTE_COUNT = VOTE_COUNT + 1 WHERE USER_ID IS ?";
+            } else {
+                trackVoteSQL = "INSERT INTO " + USER_TABLE_NAME + "(USER_ID, VOTE_COUNT) VALUES (?, 1)";
+            }
+
+
+            // Disable auto-committing, so we can treat the updates as a transaction
+            c.setAutoCommit(false);
+
+            castVoteStmt = c.prepareStatement(castVoteSQL);
+            castVoteStmt.setString(1, candidateId);
+
+            trackVoteStmt = c.prepareStatement(trackVoteSQL);
+            trackVoteStmt.setString(1, userId);
+
+            // Cast the vote and track it in our user table
+            castRow = castVoteStmt.executeUpdate();
+            castVoteStmt.close();
+            trackRow = trackVoteStmt.executeUpdate();
+            trackVoteStmt.close();
+
+
+            logger.info("Voted successfully");
+
+        } catch (SQLException e) {
+            // Something unexpected happened
+            logger.severe("Error when populating vote count table. Specifically an error with SQL.");
+            c.rollback();
+            e.printStackTrace();
+            return 2;
+        } catch (ClassNotFoundException e) {
+            // The SQL driver isn't available. This causes problems.
+            logger.severe("Error when populating vote count table. Specifically: SQL JDBC driver not found.");
+            e.printStackTrace();
+            return 2;
+        } catch (Exception e) {
+            // Something else when wrong
+            logger.severe("Error when populating vote count table.");
+            e.printStackTrace();
+            return 2;
+        } finally {
+            // Close out the connection and transaction
+            c.commit();
+            c.setAutoCommit(oldAutoCommit);
+            c.close();
+        }
+
+        if (trackRow == 1 && castRow == 1) {
+            return 0;
+        }
+
+        return 3;
     }
 }
